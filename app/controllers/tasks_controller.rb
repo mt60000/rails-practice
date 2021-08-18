5cbba771dbe84cf089712043763140d5b137297c
mt60000 <niruyaku45@gmail.com>
2021-08-18 09:21:46 +0000

    7章終了

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -4,7 +4,12 @@ class TasksController < ApplicationController
 
   def index
     @q = current_user.tasks.ransack(params[:q])
-    @tasks = @q.result(distinct: true)
+    @tasks = @q.result(distinct: true).page(params[:page])
+
+    respond_to do |format|
+      format.html
+      format.csv { send_data @tasks.generate_csv, filename: "tasks-#{Time.zone.now.strftime('%Y%m%d%S')}.csv" }
+    end
   end
 
   def show
@@ -34,13 +39,13 @@ class TasksController < ApplicationController
   end
 
   def update
-    task.update(task_params)
-    redirect_to tasks_url, notice: "タスク「#{task.name}を更新しました。」"
+    @task.update(task_params)
+    redirect_to tasks_url, notice: "タスク「#{@task.name}を更新しました。」"
   end
 
   def destroy
-    task.destroy
-    redirect_to tasks_url, notice: "タスク「#{task.name}」を削除しました。"
+    @task.destroy
+    redirect_to tasks_url, notice: "タスク「#{@task.name}」を削除しました。"
   end
 
   def confirm_new
@@ -48,11 +53,16 @@ class TasksController < ApplicationController
     render :new unless @task.valid?
   end
 
+  def import
+    current_user.tasks.import(params[:file])
+    redirect_to tasks_url, notice: "タスクを追加しました"
+  end
+
 
   private
 
   def task_params
-    params.require(:task).permit(:name, :description)
+    params.require(:task).permit(:name, :description, :image)
   end
 
   def set_task
--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,8 @@
 require_relative 'boot'
 
+require 'csv'
 require 'rails/all'
+
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -11,6 +13,9 @@ module Taskleaf
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
     config.time_zone = 'Asia/Tokyo'
+    config.i18n.default_locale = :ja
+
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
